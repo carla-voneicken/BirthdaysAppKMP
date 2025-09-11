8dd9a6b6487dc5c33b82d5a50e1e004a135d41e6
carla-voneicken <carlavoneicken@googmail.com>
2025-09-11 17:42:07 +0200

    Initial setup including dependencies for Room, Koin, Coroutines, ObservableViewModel, NativeCoroutines, Datetime

---
+++ iosApp/iosApp/KMPObservableViewModel.swift
@@ -0,0 +1,8 @@
+//
+// Created by Carla von Eicken on 10.09.25.
+//
+
+import KMPObservableViewModelCore
+import shared // This should be your shared KMP module
+
+extension Kmp_observableviewmodel_coreViewModel: ViewModel { }
