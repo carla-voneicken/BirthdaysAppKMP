6f03082b671e6d1f38b7e68032398658b8f8aad8
carla-voneicken <carlavoneicken@googlemail.com>
2025-09-11 21:02:37 +0200

    Created BirthdaysRepository with all its methods

---
+++ shared/src/commonMain/kotlin/de/carlavoneicken/birthdaysapp/data/repositories/BirthdaysRepositoryImpl.kt
@@ -0,0 +1,50 @@
+package de.carlavoneicken.birthdaysapp.data.repositories
+
+import de.carlavoneicken.birthdaysapp.data.database.BirthdayDao
+import de.carlavoneicken.birthdaysapp.data.utils.toDomain
+import de.carlavoneicken.birthdaysapp.data.models.Birthday
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class BirthdaysRepositoryImpl(private val birthdayDao: BirthdayDao): BirthdaysRepository {
+
+    override fun observeSingleBirthday(id: Long): Flow<Birthday?> =
+        birthdayDao.observeSingleBirthday(id)
+            .map { entity -> entity?.toDomain() }
+
+    override fun observeBirthdaysSortedByName(): Flow<List<Birthday>> =
+        birthdayDao.observeBirthdaysSortedByName()
+            .map { entities -> entities.map { it.toDomain() } }
+
+    override fun observeBirthdaysSortedByUpcoming(): Flow<List<Birthday>> =
+        birthdayDao.observeAllBirthdays()
+            .map { entities -> entities.map { it.toDomain() }
+                .sortedBy { it.nextBirthday } }
+
+    override suspend fun createBirthday(birthday: Birthday): Result<Unit> {
+        return try {
+            birthdayDao.createBirthday(birthday)
+            Result.success(Unit)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+
+    override suspend fun updateBirthday(birthday: Birthday): Result<Unit> {
+        return try {
+            birthdayDao.updateBirthday(birthday)
+            Result.success(Unit)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+
+    override suspend fun deleteBirthday(birthday: Birthday): Result<Unit> {
+        return try {
+            birthdayDao.deleteBirthday(birthday)
+            Result.success(Unit)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+}
