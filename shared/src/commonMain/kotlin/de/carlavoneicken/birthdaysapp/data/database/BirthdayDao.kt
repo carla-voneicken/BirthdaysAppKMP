7caaa236cd2ad10d7c7ccab5c49f2bcde5c881fb
carla-voneicken <carlavoneicken@googlemail.com>
2025-09-11 21:02:22 +0200

    Created BirthdayDao queries

--- shared/src/commonMain/kotlin/de/carlavoneicken/birthdaysapp/data/database/BirthdayDao.kt
+++ shared/src/commonMain/kotlin/de/carlavoneicken/birthdaysapp/data/database/BirthdayDao.kt
@@ -1,7 +1,31 @@
 package de.carlavoneicken.birthdaysapp.data.database
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import de.carlavoneicken.birthdaysapp.data.models.Birthday
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface BirthdayDao {
+
+    @Query("SELECT * FROM birthdays WHERE id = :id")
+    fun observeSingleBirthday(id: Long): Flow<BirthdayEntity?>
+
+    @Query("SELECT * FROM birthdays")
+    fun observeAllBirthdays(): Flow<List<BirthdayEntity>>
+
+    @Query("SELECT * FROM birthdays ORDER BY name")
+    fun observeBirthdaysSortedByName(): Flow<List<BirthdayEntity>>
+
+    @Insert
+    suspend fun createBirthday(birthday: Birthday)
+
+    @Update
+    suspend fun updateBirthday(birthday: Birthday)
+
+    @Delete
+    suspend fun deleteBirthday(birthday: Birthday)
 }
