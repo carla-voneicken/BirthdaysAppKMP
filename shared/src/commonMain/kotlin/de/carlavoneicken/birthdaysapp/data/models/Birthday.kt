3eb512327d3ddb6839d119e8ad53c15f6f61704f
carla-voneicken <carlavoneicken@googmail.com>
2025-09-11 17:43:21 +0200

    Data class for Birthday, enum for ZodiacSign and their helper functions

---
+++ shared/src/commonMain/kotlin/de/carlavoneicken/birthdaysapp/data/models/Birthday.kt
@@ -0,0 +1,30 @@
+package de.carlavoneicken.birthdaysapp.data.models
+
+import de.carlavoneicken.birthdaysapp.data.utils.getNextAge
+import de.carlavoneicken.birthdaysapp.data.utils.getNextBirthday
+import de.carlavoneicken.birthdaysapp.data.utils.getZodiacSign
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.todayIn
+import kotlin.time.Clock
+import kotlin.time.ExperimentalTime
+
+@OptIn(ExperimentalTime::class)
+data class Birthday(
+    val id: Long,
+    val name: String,
+    val day: Int,
+    val month: Int,
+    val year: Int? = null
+) {
+    val zodiacSign: ZodiacSign?
+        get() = getZodiacSign(month, day)
+
+    val nextBirthday: LocalDate
+        get() = getNextBirthday(month, day) ?: Clock.System.todayIn(TimeZone.currentSystemDefault())
+
+    val nextAge: Int?
+        get() = getNextAge(year, month, day)
+}
+
+
