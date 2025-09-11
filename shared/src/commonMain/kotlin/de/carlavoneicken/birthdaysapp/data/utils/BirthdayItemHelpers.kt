37e50048922f2f187061e3cb8537320f14d258ae
carla-voneicken <carlavoneicken@googlemail.com>
2025-09-11 17:43:21 +0200

    Data class for Birthday, enum for ZodiacSign and their helper functions

---
+++ shared/src/commonMain/kotlin/de/carlavoneicken/birthdaysapp/data/utils/BirthdayItemHelpers.kt
@@ -0,0 +1,45 @@
+@file:OptIn(ExperimentalTime::class)
+
+package de.carlavoneicken.birthdaysapp.data.utils
+
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.todayIn
+import kotlin.time.Clock
+import kotlin.time.ExperimentalTime
+
+fun getNextBirthday(month: Int, day: Int) : LocalDate? {
+    val timeZone = TimeZone.currentSystemDefault()
+    val today = Clock.System.todayIn(timeZone)
+    val currentYear: Int = today.year
+
+    // create LocalDate for the birthday this year (catch errors, e.g. Feb 29th in a non-leap year)
+    val birthdayThisYear = try {
+        LocalDate(currentYear, month, day)
+    } catch (e: IllegalArgumentException) {
+        return null
+    }
+
+    return when {
+        // if birthday is TODAY, show birthdayThisYear as next birthday
+        today == birthdayThisYear -> birthdayThisYear
+        // if today is after birthday (birthday already happened this year) add a year to it
+        today > birthdayThisYear -> try {
+            LocalDate(currentYear + 1, month, day)
+        } catch (e: IllegalArgumentException) {
+            null
+        }
+        // else (birthday still coming this year) return birthdayThisYear
+        else -> birthdayThisYear
+    }
+}
+
+fun getNextAge(year: Int?, month: Int, day: Int) : Int? {
+    val nextBirthday = getNextBirthday(month, day)
+
+    return if (year != null && nextBirthday != null) {
+        nextBirthday.year - year
+    } else {
+        null
+    }
+}
