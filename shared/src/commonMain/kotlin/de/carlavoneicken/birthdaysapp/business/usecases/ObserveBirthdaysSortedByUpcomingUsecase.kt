256c4aa658b1e76fa3b651196f454978e0ef80b0
carla-voneicken <carlavoneicken@googmail.com>
2025-09-23 09:18:19 +0200

    Added @NativeCoroutines to the suspend functions in the usecases

--- shared/src/commonMain/kotlin/de/carlavoneicken/birthdaysapp/business/usecases/ObserveBirthdaysSortedByUpcomingUsecase.kt
+++ shared/src/commonMain/kotlin/de/carlavoneicken/birthdaysapp/business/usecases/ObserveBirthdaysSortedByUpcomingUsecase.kt
@@ -1,10 +1,12 @@
 package de.carlavoneicken.birthdaysapp.business.usecases
 
+import com.rickclephas.kmp.nativecoroutines.NativeCoroutines
 import de.carlavoneicken.birthdaysapp.data.models.Birthday
 import de.carlavoneicken.birthdaysapp.data.repositories.BirthdaysRepository
 import kotlinx.coroutines.flow.Flow
 
 class ObserveBirthdaysSortedByUpcomingUsecase(private val repo: BirthdaysRepository) {
+    @NativeCoroutines
     operator fun invoke(): Flow<List<Birthday>> {
         return repo.observeBirthdaysSortedByUpcoming()
     }
