102e3685e68789e72eca16390c1b94833c739bfb
carla-voneicken <carlavoneicken@googlemail.com>
2025-09-23 09:18:19 +0200

    Added @NativeCoroutines to the suspend functions in the usecases

--- shared/src/commonMain/kotlin/de/carlavoneicken/birthdaysapp/business/usecases/DeleteBirthdayUsecase.kt
+++ shared/src/commonMain/kotlin/de/carlavoneicken/birthdaysapp/business/usecases/DeleteBirthdayUsecase.kt
@@ -1,9 +1,11 @@
 package de.carlavoneicken.birthdaysapp.business.usecases
 
+import com.rickclephas.kmp.nativecoroutines.NativeCoroutines
 import de.carlavoneicken.birthdaysapp.data.models.Birthday
 import de.carlavoneicken.birthdaysapp.data.repositories.BirthdaysRepository
 
 class DeleteBirthdayUsecase(private val repo: BirthdaysRepository) {
+    @NativeCoroutines
     suspend operator fun invoke(birthday: Birthday): Result<Unit> {
         return repo.deleteBirthday(birthday)
     }
--- shared/src/commonMain/kotlin/de/carlavoneicken/birthdaysapp/business/usecases/ObserveSingleBirthdayUsecase.kt
+++ shared/src/commonMain/kotlin/de/carlavoneicken/birthdaysapp/business/usecases/ObserveSingleBirthdayUsecase.kt
@@ -1,10 +1,12 @@
 package de.carlavoneicken.birthdaysapp.business.usecases
 
+import com.rickclephas.kmp.nativecoroutines.NativeCoroutines
 import de.carlavoneicken.birthdaysapp.data.models.Birthday
 import de.carlavoneicken.birthdaysapp.data.repositories.BirthdaysRepository
 import kotlinx.coroutines.flow.Flow
 
 class ObserveSingleBirthdayUsecase(private val repo: BirthdaysRepository) {
+    @NativeCoroutines
     suspend operator fun invoke(id: Long): Flow<Birthday?> {
         return repo.observeSingleBirthday(id)
     }
