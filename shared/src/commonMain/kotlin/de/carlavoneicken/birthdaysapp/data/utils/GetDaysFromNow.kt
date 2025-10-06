6b0f040205e60ce089c8961a8a99fd81c0e29bd8
carla-voneicken <carlavoneicken@googmail.com>
2025-10-06 21:05:28 +0200

    Created helper function to calculate the day from now until a given birthday, also created an expect/actual GetSystemTimeZone function to make it work on all platforms

--- shared/src/commonMain/kotlin/de/carlavoneicken/birthdaysapp/data/utils/GetDaysFromNow.kt
+++ shared/src/commonMain/kotlin/de/carlavoneicken/birthdaysapp/data/utils/GetDaysFromNow.kt
@@ -1,7 +1,6 @@
 package de.carlavoneicken.birthdaysapp.data.utils
 
 import kotlinx.datetime.LocalDate
-import kotlinx.datetime.TimeZone
 import kotlinx.datetime.daysUntil
 import kotlinx.datetime.todayIn
 import kotlin.time.Clock
@@ -11,10 +10,9 @@ import kotlin.time.ExperimentalTime
 @OptIn(ExperimentalTime::class)
 fun getDaysFromNow(date: LocalDate): String {
     // Get today as a LocalDate in the current system time zone
-    val today: LocalDate = Clock.System.todayIn(TimeZone.currentSystemDefault())
+    val today: LocalDate = Clock.System.todayIn(getSystemTimeZone())
 
-    // Difference in days between start-of-day(today) and start-of-day(date)
-    val days = today.daysUntil(date) // positive if `date` is in the future
+    val days = today.daysUntil(date)
 
     return when (days) {
         0 -> "Heute"
