2ed9734ad15e98d421a4974e4a89d0516e0ce20c
carla-voneicken <carlavoneicken@googlemail.com>
2025-10-06 21:02:41 +0200

    Created a test repository for sample data

---
+++ shared/src/commonTest/kotlin/de/carlavoneicken/birthdaysapp/data/database/SampleBirthdays.kt
@@ -0,0 +1,12 @@
+package de.carlavoneicken.birthdaysapp.data.database
+
+import de.carlavoneicken.birthdaysapp.data.models.Birthday
+
+val sampleBirthdays = listOf(
+    Birthday(name = "Alice", day = 15, month = 1, year = 1990),
+    Birthday(name = "Bob", day = 6, month = 10, year = 1985),
+    Birthday(name = "Carol", day = 29, month = 2, year = 2000),
+    Birthday(name = "Dave", day = 31, month = 12, year = 1993),
+    Birthday(name = "Eric", day = 7, month = 10),
+    Birthday(name = "Fred", day = 29, month = 2)
+)
