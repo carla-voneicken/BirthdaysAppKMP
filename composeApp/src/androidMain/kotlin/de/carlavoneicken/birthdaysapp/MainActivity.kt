6d70b954c7394f0e555572c8dd7a6681fe48278b
carla-voneicken <carlavoneicken@googlemail.com>
2025-10-17 09:00:54 +0200

    Added a custom theme in Android

--- composeApp/src/androidMain/kotlin/de/carlavoneicken/birthdaysapp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/de/carlavoneicken/birthdaysapp/MainActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import de.carlavoneicken.birthdaysapp.presentation.BirthdaysListView
 
@@ -15,21 +19,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            App()
+            MaterialTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    BirthdaysListView()
+                }
+            }
         }
     }
-}
-
-@Composable
-fun App() {
-    // Optionally apply your theme
-    //BirthdayTheme {
-    BirthdaysListView()
-    //}
-}
-
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
 }
