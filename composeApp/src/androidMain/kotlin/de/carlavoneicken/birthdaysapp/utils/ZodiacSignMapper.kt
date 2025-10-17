9d52237cc0792ab48919b6643487432c4c0f4704
carla-voneicken <carlavoneicken@googlemail.com>
2025-10-17 13:31:33 +0200

    Edited ZodiacSign icons to be more crisp

--- composeApp/src/androidMain/kotlin/de/carlavoneicken/birthdaysapp/utils/ZodiacSignMapper.kt
+++ composeApp/src/androidMain/kotlin/de/carlavoneicken/birthdaysapp/utils/ZodiacSignMapper.kt
@@ -6,16 +6,16 @@ import de.carlavoneicken.birthdaysapp.data.models.ZodiacSign
 
 @DrawableRes
 fun ZodiacSign.toDrawableRes(): Int = when (this) {
-    ZodiacSign.CAPRICORN -> R.drawable.capricorn_
-    ZodiacSign.AQUARIUS -> R.drawable.aquarius_
-    ZodiacSign.PISCES -> R.drawable.pisces_
-    ZodiacSign.ARIES -> R.drawable.aries_
-    ZodiacSign.TAURUS -> R.drawable.taurus_
-    ZodiacSign.GEMINI -> R.drawable.gemini_
-    ZodiacSign.CANCER -> R.drawable.cancer_
-    ZodiacSign.LEO -> R.drawable.leo_
-    ZodiacSign.VIRGO -> R.drawable.virgo_
-    ZodiacSign.LIBRA -> R.drawable.libra_
-    ZodiacSign.SCORPIO -> R.drawable.scorpio_
-    ZodiacSign.SAGITTARIUS -> R.drawable.sagittarius_
+    ZodiacSign.CAPRICORN -> R.drawable.ic_capricorn_
+    ZodiacSign.AQUARIUS -> R.drawable.ic_aquarius_
+    ZodiacSign.PISCES -> R.drawable.ic_pisces_
+    ZodiacSign.ARIES -> R.drawable.ic_aries_
+    ZodiacSign.TAURUS -> R.drawable.ic_taurus_
+    ZodiacSign.GEMINI -> R.drawable.ic_gemini_
+    ZodiacSign.CANCER -> R.drawable.ic_cancer_
+    ZodiacSign.LEO -> R.drawable.ic_leo_
+    ZodiacSign.VIRGO -> R.drawable.ic_virgo_
+    ZodiacSign.LIBRA -> R.drawable.ic_libra_
+    ZodiacSign.SCORPIO -> R.drawable.ic_scorpio_
+    ZodiacSign.SAGITTARIUS -> R.drawable.ic_sagittarius_
 }
