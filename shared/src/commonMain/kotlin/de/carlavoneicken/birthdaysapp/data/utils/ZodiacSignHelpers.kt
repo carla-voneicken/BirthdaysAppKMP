df6c2fca2bd3d9214d8cd8f90eb052217db06608
carla-voneicken <carlavoneicken@googlemail.com>
2025-10-17 08:56:25 +0200

    Corrected some dates of the zodiac sign ranges

--- shared/src/commonMain/kotlin/de/carlavoneicken/birthdaysapp/data/utils/ZodiacSignHelpers.kt
+++ shared/src/commonMain/kotlin/de/carlavoneicken/birthdaysapp/data/utils/ZodiacSignHelpers.kt
@@ -11,12 +11,12 @@ fun getZodiacSign(month: Int, day: Int): ZodiacSign? {
         Triple(LocalDate(2000, 2, 19), LocalDate(2000, 3, 20), ZodiacSign.PISCES),
         Triple(LocalDate(2000, 3, 21), LocalDate(2000, 4, 19), ZodiacSign.ARIES),
         Triple(LocalDate(2000, 4, 20), LocalDate(2000, 5, 20), ZodiacSign.TAURUS),
-        Triple(LocalDate(2000, 5, 21), LocalDate(2000, 6, 20), ZodiacSign.GEMINI),
-        Triple(LocalDate(2000, 6, 21), LocalDate(2000, 7, 22), ZodiacSign.CANCER),
+        Triple(LocalDate(2000, 5, 21), LocalDate(2000, 6, 21), ZodiacSign.GEMINI),
+        Triple(LocalDate(2000, 6, 22), LocalDate(2000, 7, 22), ZodiacSign.CANCER),
         Triple(LocalDate(2000, 7, 23), LocalDate(2000, 8, 22), ZodiacSign.LEO),
         Triple(LocalDate(2000, 8, 23), LocalDate(2000, 9, 22), ZodiacSign.VIRGO),
-        Triple(LocalDate(2000, 9, 23), LocalDate(2000, 10, 22), ZodiacSign.LIBRA),
-        Triple(LocalDate(2000, 10, 23), LocalDate(2000, 11, 21), ZodiacSign.SCORPIO),
+        Triple(LocalDate(2000, 9, 23), LocalDate(2000, 10, 23), ZodiacSign.LIBRA),
+        Triple(LocalDate(2000, 10, 24), LocalDate(2000, 11, 21), ZodiacSign.SCORPIO),
         Triple(LocalDate(2000, 11, 22), LocalDate(2000, 12, 21), ZodiacSign.SAGITTARIUS)
     )
 
