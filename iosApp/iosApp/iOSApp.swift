05ce90a4052bb3fa611a873aeb51421643164774
carla-voneicken <carlavoneicken@googlemail.com>
2025-10-28 14:30:10 +0100

    Minor changes

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -5,12 +5,6 @@ import Shared
 struct iOSApp: App {
     init() {
         KoinHelperKt.doInitKoinIos(useFakeData: true)
-        let launchSB = Bundle.main.object(forInfoDictionaryKey: "UILaunchStoryboardName") as? String
-        let launchDict = Bundle.main.object(forInfoDictionaryKey: "UILaunchScreen") as? [String: Any]
-        print("UILaunchStoryboardName =", launchSB ?? "nil")
-        print("UILaunchScreen keys =", Array((launchDict ?? [:]).keys))
-        let dict = Bundle.main.infoDictionary ?? [:]
-        print("UILaunchScreen =", dict["UILaunchScreen"] ?? "nil")
     }
 
     var body: some Scene {
