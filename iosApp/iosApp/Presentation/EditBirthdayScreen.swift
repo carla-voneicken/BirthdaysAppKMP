2bb6db91b9b582a382f7940d796272327a8edd8d
carla-voneicken <carlavoneicken@googlemail.com>
2025-11-12 11:16:18 +0100

    Created EditBirthdayScreen (no actual content yet)

---
+++ iosApp/iosApp/Presentation/EditBirthdayScreen.swift
@@ -0,0 +1,19 @@
+//
+//  EditBirthdayScreen.swift
+//  iosApp
+//
+//  Created by Carla von Eicken on 31.10.25.
+//
+
+import SwiftUI
+
+struct EditBirthdayScreen: View {
+    let birthdayId: Int64
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    EditBirthdayScreen(birthdayId: 1)
+}
